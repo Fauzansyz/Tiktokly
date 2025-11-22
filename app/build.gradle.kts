944902ec18e4dfa5558d45105fcbbaf4be36a893
Fauzansyz <fauzanadzma2@gmail.com>
2025-11-22 20:34:02 +0700

    add release variant & bug fixes & optimizing performance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,69 @@
+import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    
-  id("kotlin-android")
-  
-  id("com.google.devtools.ksp")
-  
+    id("kotlin-android")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.zandeveloper.tiktokly"
     compileSdk = 35
-    
-    
+
     defaultConfig {
         applicationId = "com.zandeveloper.tiktokly"
         minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-        
-        vectorDrawables { 
+
+        vectorDrawables {
             useSupportLibrary = true
         }
-        
     }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+
+    // -------------------------
+    // LOAD key.properties
+    // -------------------------
+    val keystoreProps = Properties()
+    val keystoreFile = rootProject.file("key.properties")
+
+    if (keystoreFile.exists()) {
+        FileInputStream(keystoreFile).use { keystoreProps.load(it) }
+    }
+    // -------------------------
+
+    signingConfigs {
+        create("release") {
+            if (keystoreFile.exists()) {
+                storeFile = rootProject.file(keystoreProps.getProperty("storeFile"))
+                storePassword = keystoreProps.getProperty("storePassword")
+                keyAlias = keystoreProps.getProperty("keyAlias")
+                keyPassword = keystoreProps.getProperty("keyPassword")
+            }
+        }
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
-    
+
     buildFeatures {
         viewBinding = true
-        
     }
-    
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
@@ -52,18 +73,17 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.github.Spikeysanju:MotionToast:1.4")
 
-implementation("com.squareup.okhttp3:okhttp:4.11.0")
-implementation("com.google.code.gson:gson:2.11.0")
-implementation("com.github.Spikeysanju:MotionToast:1.4")
-	
-     implementation("com.google.android.gms:play-services-ads:22.6.0")
+    implementation("com.google.android.gms:play-services-ads:22.6.0")
 
-     implementation("com.github.bumptech.glide:glide:4.16.0")
-ksp("com.github.bumptech.glide:ksp:4.16.0")
-     
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:ksp:4.16.0")
+
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    
+
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
